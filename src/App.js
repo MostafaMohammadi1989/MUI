14b57c33bd9551b08c1ea1abf83474c5ce0b3be9
mostafamohammadi1989 <mostafa.mohammadi.1989.10.26@gmail.com>
2023-03-08 10:58:15 +0330

    setup project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import Button from "@mui/material/Button";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Meisam</h1>
+      <Button>Click Me</Button>
+      <Button variant="contained">Click Me</Button>
+      <Button variant="outlined">Click Me</Button>
+      <Button>Click Me</Button>
     </div>
   );
 }
