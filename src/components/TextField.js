fe736d8c3e5662f584bb5e55fa028bdb1ac7ff3c
mostafamohammadi1989 <mostafa.mohammadi.1989.10.26@gmail.com>
2023-03-11 03:27:46 +0330

    TextField in MUI

---
+++ src/components/TextField.js
@@ -0,0 +1,17 @@
+import * as React from "react";
+import { Box, TextField } from "@mui/material";
+
+export default function TextFields() {
+  return (
+    <Box
+      component="form"
+      sx={{
+        "& > :not(style)": { m: 1, width: "25ch" },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+    </Box>
+  );
+}
