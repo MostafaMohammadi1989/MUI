9917b3d6bb29f9bb5a25f738a427bf6e9e9935aa
mostafamohammadi1989 <mostafa.mohammadi.1989.10.26@gmail.com>
2023-03-11 17:36:51 +0330

    use the difftern textfield and use state

--- src/components/TextField.js
+++ src/components/TextField.js
@@ -1,7 +1,10 @@
 import * as React from "react";
-import { Box, TextField } from "@mui/material";
+import { Box, TextField, Typography } from "@mui/material";
+import styled, { css } from "styled-components";
+import { useState } from "react";
 
 export default function TextFields() {
+  const [name, setName] = useState("");
   return (
     <Box
       component="form"
@@ -14,6 +17,45 @@ export default function TextFields() {
       <TextField id="outlined-basic" label="Outlined" variant="outlined" />
       <TextField id="filled-basic" label="Filled" variant="filled" />
       <TextField id="standard-basic" label="Standard" variant="standard" />
+      <Box
+        component="div"
+        border={1}
+        borderColor="primary.main"
+        // borderRadius={16}
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          // justifyContent: "center",
+          // alignItems: "center",
+          margin: "0 auto",
+        }}
+      >
+        <TextField
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          sx={margintText}
+          type={"text"}
+          placeholder="Username"
+          variant="outlined"
+        />
+        <TextField
+          sx={margintText}
+          type={"email"}
+          placeholder="Email"
+          variant="standard"
+        />
+        <TextField
+          sx={margintText}
+          type={"password"}
+          placeholder="Password"
+          variant="filled"
+        />
+        <Typography>{name}</Typography>
+      </Box>
     </Box>
   );
 }
+
+const margintText = css`
+  margin: 20px;
+`;
